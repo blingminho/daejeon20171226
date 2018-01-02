a422a7cc55d3ba6ef7f829638d13bdc887b9f3cf
blingminho <tkdjun67@naver.com>
2018-01-02 11:55:51 +0900

    연산자 항목 증감 부호 비트전환 논리부정 산술 - 사칙, 나머지, 쉬프트 비교 - 대소비교, 등가비교, 비트, 논리 삼항 대입 Math.random() 반올림

--- src/basic_1228/OperationBasic02.java
+++ src/basic_1228/OperationBasic02.java
@@ -83,7 +83,7 @@ public class OperationBasic02 {
 		 *  : byte + char -> int + int
 		 * - 두개의 피연사중 자료형의 범위가 큰 쪽에 맞춰서 변환후 연산한다.
 		 *  : int + float -> float + float
-		 * - 정수형간의 나눗셈에서 0으로 나누는 것은 금지되어 있다. 
+		 * - 정수형간의 나눗셈에서 0으로 나누는 것은 금지되어 있다.
 		 */
 		
 		byte aa = 1;
@@ -174,8 +174,8 @@ public class OperationBasic02 {
 		 * 13. 논리연산자 ( &&(AND결합), ||(OR결합))
 		 * - 피연산자로 boolean형 또는 boolean형 값을 결과로 하는 조건식만 허용한다.
 		 * - 조건식의 결합에 사용한다.
-		 * - &&(AND 결합) : 둘다 true 일때 true
-		 * - ||(OR 결합) : 둘중 하나만 true이면 true
+		 * - &&(AND 결합) : 둘다 true 일때 true (* 같음 - 우선순위 : || 보다 빠름)
+		 * - ||(OR 결합) : 둘중 하나만 true이면 true (+ 같음)
 		 */
 		//영어 소문자 이면 true를 출력해주는 출력문을 만들겠습니다.
 		char c = 'f';
@@ -190,8 +190,85 @@ public class OperationBasic02 {
 		//숫자 인지 판단
 		System.out.println('0'<=c && c<= '9');
 		
+		/*
+		 * ##20180102##
+		 * 14. 삼항연산자
+		 * - 세 개의 피연산자라를 필요로 하기 때문에 삼항 연산자로 이름지어졌다.
+		 * - 기본구조
+		 * 		(조건식) ? 식1 : 식2
+		 * String result = 5 < 10 ? "5가 10보다 크다" : "5가 10보다 작다"
+		 */
+		int x = -10;
+		int absx = (x >= 0) ? x : -x;// 절대값 구하는 식
+		System.out.println(absx);
+		// x가 0이면 0을, 양수이면 "양수", 음수이면 "음수" 출력하도록 만드세요.
+		System.out.println( (x > 0) ? "양수" : ( (x == 0) ? "0" : "음수") );
+		
+		// 로직을 일단 한글로 쓰고 나중에 코딩으로 옮긴다!!!!!!!!
 		
 		
+		/*
+		 * 15. 대입 연산자 ( =, op=)
+		 * - 변수에 값 또는 수식의 연산결과를 저장하는데 사용한다.
+		 * - 연산자중 가장 낮은 연산우선순위를 가진다.
+		 */
+		int i7 = 0;
+		i7 = 4;
+		
+		i7 = i7 + 5;
+		i7 += 5;
+		
+		i7 /= 2;
+		i7 = i7 / 2;
+		
+		/*
+		 * 16. Math.random()
+		 * 0 <= M < 1 (double 타입 반환)
+		 * 앞에는 포함, 뒤에는 미포함
+		 * 23(포함) ~ 147(미포함) 경우 147 - 23 == 124 ==> *124
+		 * 실수형 ==> 정수형
+		 * 23부터 시작 ==> +23
+		 * 
+		 * 
+		 * 주사위 경우 6개 1 ~ 6
+		 * Math.random() * 6 == ( 0 <= x < 6 사이의 실수형)
+		 * (int)(Math.random() * 6) == ( 0 <= x < 6 사이의 정수형)
+		 * (int)(Math.random() * 6) + 1 == ( 1 <= x < 7 사이의 정수형)
+		 * int dice = (int)(Math.random()*6 + 1);
+		 */
+		int dice = (int)(Math.random()*6 + 1);
+		System.out.println(dice);
+		
+		// 로또번호 1 ~ 45
+		int lottoNum = (int)(Math.random()*45) + 1;
+		System.out.println("로또번호 : " + lottoNum);
+		
+		// 23(미포함) ~ 101(포함)
+		// 24 포함 ~ 102 미포함 // 102-24 == 78
+		int ranNum = (int)(Math.random()*78) + 24;
+		System.out.println(ranNum);
+		
+		/*
+		 * 17. 반올림
+		 * 
+		 * 
+		 * 
+		 * 
+		 */
+		double b2 = 3.141592;
+		int sub = 1000;
+		float f2 = (int)(b2 * 1000 + 0.5) / (float)1000;
+		float f3 = (int)(b2 * sub + 0.5) / (float)sub;
+		System.out.println(f2);
+		System.out.println(f3);
+		
+		// 285.3984
+		// 소수점 셋째자리에서 반올림하여 두번째 자리까지 표현하세요
+		// 원하는 답 285.40 => *100 -> 28539.84 -> 28539.84 + 0.5 == 28540.34 -> (int)형 캐스팅 -> 28540 -> /(float)100 -> 285.40
+		float b4 = 285.3984f;
+		int sub2 = 100;
+		float f4 = (int)(b4 * sub2 + 0.5) / (float)sub2;
+		System.out.println(f4);// 285.40f == 285.4f
 		
 		
 	}
