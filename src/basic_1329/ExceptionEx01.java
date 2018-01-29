6eefe29d3f724d93f39da9f9bd76f59fcd8c4de3
blingminho <tkdjun67@naver.com>
2018-01-29 13:38:21 +0900

    * 예외처리 연습1  * try-catch문  * @author SangJun

---
+++ src/basic_1329/ExceptionEx01.java
@@ -0,0 +1,29 @@
+package basic_1329;
+/**
+ * 예외처리 연습1
+ * try-catch문
+ * @author SangJun
+ *
+ */
+public class ExceptionEx01 {
+	public static void main(String[] args) {
+		int number = 100;
+		int result = 0;
+		/*
+		for(int i = 0; i < 10; i++){
+			result = number / (int)(Math.random()*5);
+			System.out.println(result);
+		}
+		*/
+		for(int i = 0; i < 10; i++){
+			try{
+				result = number / (int)(Math.random()*5);
+				System.out.println(result);
+			} catch(ArithmeticException e){
+				System.out.println("0으로 나누지마!");
+			}
+		}
+		System.out.println("종료");
+		
+	}
+}
